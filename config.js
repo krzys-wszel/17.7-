8f178e0f2da74a59facc8b9a21772fe562a43dbb
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-04-20 20:25:24 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+    'GOOGLE_CLIENT_ID': '1007585159416-k84j9v8c9amveenjrqn01jopjo5f6pqi.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'd6TU59VyhCWc8uarqfach7Q9',
+    'CALLBACK_URL': 'http://localhost:3000/auth/goole/callback'
+}
